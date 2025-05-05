67a6fc8dd316629dc51d909ad3910aeb7f95d1ce
Anthony <doantkamroza@icloud.com>
2025-05-05 17:58:45 +0200

    Initial Commit

---
+++ YouAreAwesome/YouAreAwesomeApp.swift
@@ -0,0 +1,17 @@
+//
+//  YouAreAwesomeApp.swift
+//  YouAreAwesome
+//
+//  Created by Antoni Mróz on 05/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct YouAreAwesomeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
